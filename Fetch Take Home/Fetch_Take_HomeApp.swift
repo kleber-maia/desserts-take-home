af7220edf82a0fb48e21b118131d15b73aab4715
Kleber Maia <kleber@soft.group>
2023-11-06 14:43:49 -0500

    Initial Commit

---
+++ Fetch Take Home/Fetch_Take_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Take_HomeApp.swift
+//  Fetch Take Home
+//
+//  Created by Kleber Maia on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Take_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
